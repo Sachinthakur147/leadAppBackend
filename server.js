e9515455cb3f86df719b87488b386dff0e4aa234
Sachin Thakur <65484874+Sachinthakur147@users.noreply.github.com>
2024-08-11 20:24:56 +0530

    lead backend

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const leadRoutes = require('./routes/leadRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+connectDB();
+
+app.use('/api', leadRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
