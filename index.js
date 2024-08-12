991c0fe45c8e8296d5f8d54360c59a872a729f8e
Sachin Thakur <65484874+Sachinthakur147@users.noreply.github.com>
2024-08-12 10:44:46 +0530

    file name

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ connectDB();
 
 app.use('/api', leadRoutes);
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
